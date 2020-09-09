427fdeeb2d5b37086d99ec3d14cd8bdcd7ae7765
CrazyPancakeMuncher08 <70578309+CrazyPancakeMuncher08@users.noreply.github.com>
2020-09-09 10:56:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 function SendPHP() {
-    var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      alert(xmlhttp.responseText);
-    }
-  }
-  xmlhttp.open("GET","testdata.php",true);
-  xmlhttp.send();
+    $.get("test.asp", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
 }
