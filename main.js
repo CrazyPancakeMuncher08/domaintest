629fec748fcb16505234950c5dcfb3c4a3634480
CrazyPancakeMuncher08 <70578309+CrazyPancakeMuncher08@users.noreply.github.com>
2020-09-10 09:03:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ function Vote(integer) {
     var xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
-      document.getElementById("poll").innerHTML=this.responseText;
+      alert(this.responseText)
+      //document.getElementById("poll").innerHTML=this.responseText;
     }
   }
   xmlhttp.open("GET","vote.php?vote="+integer,true);
