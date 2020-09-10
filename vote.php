9b1c604a9bb5f4a8b2c56f070c5e70eb8d292f1b
CrazyPancakeMuncher08 <70578309+CrazyPancakeMuncher08@users.noreply.github.com>
2020-09-10 09:21:11 +0200

    Update vote.php

--- vote.php
+++ vote.php
@@ -22,6 +22,24 @@ $insertvote = $yes."||".$no;
 $fp = fopen($filename,"w");
 fputs($fp,$insertvote);
 fclose($fp);
-
-echo "Hello World!";
 ?>
+
+<h2>Result:</h2>
+<table>
+<tr>
+<td>Yes:</td>
+<td><img src="poll.gif"
+width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($yes/($no+$yes),2)); ?>%
+</td>
+</tr>
+<tr>
+<td>No:</td>
+<td><img src="poll.gif"
+width='<?php echo(100*round($no/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($no/($no+$yes),2)); ?>%
+</td>
+</tr>
+</table>
