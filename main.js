ae2ead67a0406e1f1959598e46686a84cc802779
CrazyPancakeMuncher08 <70578309+CrazyPancakeMuncher08@users.noreply.github.com>
2020-09-10 08:45:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function SendPHP() {
-    $.get("test.asp", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
+function Vote(integer) {
+    var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","vote.php?vote="+integer,true);
+  xmlhttp.send();
 }
